c75c197879e6f48230102bf9949e75fc0cc885e7
stetteh <stettehq@gmail.com>
2016-04-14 20:20:46 -0500

    Posts controller & class, comment class

---
+++ GetIt/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GetIt.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public DateTime CommentDate { get; set; }
+        public int Upvote { get; set; }
+        public int Downvote { get; set; }
+    }
+}
---
+++ GetIt/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GetIt.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime PostDate { get; set; }
+        public int Upvote { get; set; }
+        public int Downvote { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
