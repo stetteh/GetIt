3903860d91b8447357406660870ed2e75ed0a656
stetteh <stettehq@gmail.com>
2016-04-15 19:46:42 -0500

    Comment class

--- GetIt/Models/Comment.cs
+++ GetIt/Models/Comment.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GetIt.Models
 {
     public class Comment
     {
         public int Id { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Body { get; set; }
         public DateTime CommentDate { get; set; }
+        public Post Post { get; set; }
         public int Upvote { get; set; }
         public int Downvote { get; set; }
     }
